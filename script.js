ba1e9b36f83ae2392d263f4d8081fb250c1036ff
herissonfei <89328999+herissonfei@users.noreply.github.com>
2022-04-12 16:31:06 -0400

    activité8

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 document.querySelector('#button').addEventListener('click', logMessages)
 
-function logDebug() {
-    logMessages()
-}
-
 function logMessages() {
     console.log('Message');
     console.warn('Attention!');
